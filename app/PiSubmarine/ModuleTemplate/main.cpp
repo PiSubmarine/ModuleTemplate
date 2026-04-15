383240fd9f220cb7178a24ae192c57fa99729dbe
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-15 22:57:29 +0200

    Codestyle defined

--- app/PiSubmarine/ModuleTemplate/main.cpp
+++ app/PiSubmarine/ModuleTemplate/main.cpp
@@ -1,11 +1,12 @@
 #include <spdlog/spdlog.h>
+
 #include "PiSubmarine/ModuleTemplate/ModuleTemplate.h"
 
 using namespace PiSubmarine::ModuleTemplate;
 
 int main(int argc, char* argv[])
 {
-    spdlog::info("PiSubmarine::ModuleTemplate");
-    ModuleTemplate moduleTemplate;
-    return moduleTemplate.GetReturnCode();
+	spdlog::info("PiSubmarine::ModuleTemplate");
+	ModuleTemplate moduleTemplate;
+	return moduleTemplate.GetReturnCode();
 }
